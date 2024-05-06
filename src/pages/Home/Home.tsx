de6998c6a6d6e2344a1db223b79c7b19383c1716
Manoel Farias <manoelfpjunior@hotmail.com>
2024-05-05 22:46:01 -0300

    feat: added input search and filtros

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Alert, Header } from "@/components";
-import { RotateCw, CircleAlert } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { RotateCw, CircleAlert, Search, ListFilter } from "lucide-react";
 
 const alertDescription =
   "Você pode consultar a lista de abrigos disponíveis e os itens que necessitam de doações.";
@@ -19,6 +20,19 @@ const Home = () => {
           description={alertDescription}
           startAdornment={<CircleAlert size={20} />}
         />
+        <div className="relative">
+          <Input
+            placeholder="Buscar por abrigo ou endereço"
+            className="h-[48px] text-sm font-medium text-[#8C94A4] pl-10 pr-4"
+          />
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <Search name="search" size="20" className="text-gray-400" />
+          </div>
+        </div>
+        <div className="flex gap-2 text-blue-600 items-center p-1">
+          <ListFilter />
+          <h1 className="font-semibold text-[16px]"> Filtros </h1>
+        </div>
       </div>
     </div>
   );
