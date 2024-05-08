271f9d949ff9f197991793195366e2ae8328856b
MatheusDubin <matheusdsilveira@gmail.com>
2024-05-08 00:48:58 -0300

    fix: remove hardcoded production string as fallback from no VITE_API_URL so to avoid developers making changes do production data by mistake

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log(import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'https://api.sos-rs.com',
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:4000/',
 });
 
 export { api };
