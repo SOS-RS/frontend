6afa68e303df255606162f68d25f3f114433f573
Henrique Ferreira <henriqueferreira@MacBook-Pro-de-Henrique.local>
2024-05-09 16:17:12 -0300

    feat: Add a shelter registration button at the top to make it more prominent.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,7 +24,16 @@ const Header = React.forwardRef<HTMLDivElement, IHeader>((props, ref) => {
         {startAdornment}
         <h3 className="font-medium text-white">{title}</h3>
       </div>
-      <div className="cursor-pointer ">{endAdornment}</div>
+      <div className="flex">
+        <a 
+          href="https://forms.gle/2S7L2gR529Dc8P3T9"
+          className="bg-white hover:bg-rose-50 text-red-600 font-medium py-2 px-4 rounded-full" 
+          target="_blank"
+        >
+          Cadastrar abrigo
+        </a>
+        <div className="cursor-pointer ">{endAdornment}</div>
+      </div>
     </div>
   );
 });
