20d9f104dab20d6848a06d7e162aee3c043c4634
Luccas Specht <luccas.specht.dev@gmail.com>
2024-05-09 21:47:33 -0300

    Fix: mobile UI has broken for mobile devices (#22)
    *Fix: mobile UI has broken for mobile devices
    
    * fix: remove chip as fixed as no wrap property
    
    * chore: add shelter card clickable
    
    * chore: remove Fragment component
    
    * build: add set sm as max mobile dimension
              sm: 425px
    
    * style: add responsive layout

--- src/components/ShelterListItem/ShelterListItem.tsx
+++ src/components/ShelterListItem/ShelterListItem.tsx
@@ -45,25 +45,18 @@ const ShelterListItem = (props: IShelterListItemProps) => {
 
 
   return (
-    <div className="flex flex-col p-4 w-full border-2 border-border rounded-md gap-1 relative">
-      <Button
-        size="sm"
-        variant="ghost"
-        className="absolute top-4 right-4"
-        onClick={() => navigate(`/abrigo/${data.id}`)}
-      >
+    <div
+      className="flex flex-col p-4 w-full border-2 border-border rounded-md gap-1 relative"
+      onClick={() => navigate(`/abrigo/${data.id}`)}
+    >
+      <Button size="sm" variant="ghost" className="absolute top-4 right-4">
         <ChevronRight className="h-5 w-5" />
       </Button>
       <div className="flex items-center gap-1">
-        <h3
-          className="font-semibold text-lg  hover:cursor-pointer hover:text-slate-500"
-          onClick={() => navigate(`/abrigo/${data.id}`)}
-        >
+        <h3 className="font-semibold text-lg  hover:cursor-pointer hover:text-slate-500">
           {data.name}
         </h3>
-        {data.verified && (
-          <VerifiedBadge />
-        )}
+        {data.verified && <VerifiedBadge />}
       </div>
       <h6 className={cn('font-semibold text-md', availabilityClassName)}>
         {availability}
