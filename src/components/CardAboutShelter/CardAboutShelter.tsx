5f3837976625e7c8d1d962965680e29529ce5719
kelvinsb <kelvinbr11@gmail.com>
2024-05-12 18:17:39 -0300

    feat: address fields on shelter details when theres no address field

--- src/components/CardAboutShelter/CardAboutShelter.tsx
+++ src/components/CardAboutShelter/CardAboutShelter.tsx
@@ -12,18 +12,31 @@ import { Card } from '../ui/card';
 import { ICardAboutShelter } from './types';
 import { InfoRow } from './components';
 
+const formatShelterAddressFields = (shelter: ICardAboutShelter['shelter']) => {
+  const fields = [];
+  if (shelter.street) fields.push(shelter.street);
+  if (shelter.streetNumber) fields.push(shelter.streetNumber);
+  if (shelter.neighbourhood) fields.push(shelter.neighbourhood);
+  if (shelter.zipCode) fields.push(shelter.zipCode);
+
+  return fields.join(', ');
+};
+
 const CardAboutShelter = (props: ICardAboutShelter) => {
   const { shelter } = props;
 
   const check = (v?: string | number | boolean | null) => {
     return v !== undefined && v !== null;
   };
+  const formatAddress =
+    shelter.address ??
+    `${formatShelterAddressFields(shelter)} - ${shelter.city}`;
 
   return (
     <Card className="flex flex-col gap-2 p-4 bg-[#E8F0F8] text-sm">
       <div className="text-[#646870] font-medium">Sobre o abrigo</div>
       <div className="flex flex-col flex-wrap gap-3">
-        <InfoRow icon={<Home />} label={shelter.address} />
+        <InfoRow icon={<Home />} label={formatAddress} />
         {Boolean(shelter.city) && (
           <InfoRow icon={<Building />} label={shelter.city} />
         )}
