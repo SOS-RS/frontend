a98aa9f278ce32964d1556767e1a7480e7403287
Thauã Silveira <thaua@outlook.com>
2024-05-13 12:51:30 -0300

    Add docker capability.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+    watch: {
+      usePolling: true
+    }
+  }
 });
