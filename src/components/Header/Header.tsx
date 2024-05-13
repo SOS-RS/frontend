5763358e71375554c7daf898cde1eefbed5639a9
marcoDmc <marcodamasceno07@gmail.com>
2024-05-13 15:34:13 -0300

    style: adding responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -30,6 +30,24 @@ const Header = React.forwardRef<HTMLDivElement, IHeader>((props, ref) => {
       <div className="flex items-center">
         <div className="cursor-pointer ">{endAdornment}</div>
       </div>
+
+      }
+
+      <div className='
+            absolute top-2 right-4
+            w-full max-w-10
+            h-full max-h-10
+            hidden
+            max-sm:grid
+            place-items-center
+            p-1
+            cursor-pointer
+            z-0
+            max-sm:z-20
+            ' onClick={handleStatusHamburguer}>
+        {statusHamburguer ? <span className='w-full h-1 rounded transition-all bg-neutral-100 rotate-45 translate-y-2 scale-x-105'></span> : <span className='w-full h-1 rounded bg-neutral-100 transition-all'></span>}
+        {statusHamburguer ? <span className='hidden transition-all'></span> : <span className='w-full h-1 rounded bg-neutral-100 scale-90 transition-all'></span>}
+        {statusHamburguer ? <span className='w-full h-1 rounded transition-all bg-neutral-100 -rotate-45 -translate-y-2 scale-110'></span> : <span className='w-full h-1 rounded bg-neutral-100 transition-all'></span>}
     </div>
   );
 });
