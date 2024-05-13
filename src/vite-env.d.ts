62cb1abffde28ce013b4f65a1079e62d1606e007
f3llps <fellipeversiani@gmail.com.br>
2024-05-13 17:02:19 -0300

    chore: remove unused ImportMetaEnv variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,9 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_PORT: number
-  readonly VITE_HOTJAR_SITE_ID: number
-  readonly VITE_HOTJAR_VERSION: number
-  readonly VITE_HOTJAR_DEBUG: boolean
   readonly DEV: boolean
 }
 
