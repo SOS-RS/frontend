82de0b3cc29b626f9628c6ffb8adcb3fa66cbe7f
f3llps <fellipeversiani@gmail.com.br>
2024-05-13 10:25:44 -0300

    chore: add DEV environment variable in ImportMetaEnv interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_PORT: number
+  readonly VITE_HOTJAR_SITE_ID: number
+  readonly VITE_HOTJAR_VERSION: number
+  readonly VITE_HOTJAR_DEBUG: boolean
+  readonly DEV: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
