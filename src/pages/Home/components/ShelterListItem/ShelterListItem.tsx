0eaca63d2f654030386490d8ca1281088caec10f
Pablo A. Maximo <pabloadrianomc@gmail.com>
2024-05-13 23:17:28 -0300

    fix: change shelter title style (#106)
    Prevents badge misalignment on very large titles

--- src/pages/Home/components/ShelterListItem/ShelterListItem.tsx
+++ src/pages/Home/components/ShelterListItem/ShelterListItem.tsx
@@ -43,20 +43,20 @@ const ShelterListItem = (props: IShelterListItemProps) => {
   return (
     <Link to={`/abrigo/${data.id}`} target="_blank">
       <div className="flex flex-col p-4 w-full border-2 border-border rounded-md gap-1 relative hover:bg-accent">
-        <Button size="sm" variant="ghost" className="absolute top-4 right-4">
-          <ChevronRight
-            className="h-5 w-5"
-          />
-        </Button>
-        <div
-          className="flex items-center gap-1"
-        >
-          <h3
-            className="font-semibold text-lg  hover:cursor-pointer hover:text-slate-500"
-          >
-            {data.name}
-          </h3>
-          {data.verified && <VerifiedBadge />}
+        <div className="inline-flex justify-between">
+          <div className="flex flex-row items-center gap-1">
+            <h3 className="font-semibold text-lg h-full hover:cursor-pointer hover:text-slate-500">
+              {data.name}
+            </h3>
+            {data.verified && (
+              <div className="h-full pt-1">
+                <VerifiedBadge />
+              </div>
+            )}
+          </div>
+          <Button size="sm" variant="ghost">
+            <ChevronRight className="h-5 w-5" />
+          </Button>
         </div>
         <h6 className={cn('font-semibold text-md', availabilityClassName)}>
           {availability}
