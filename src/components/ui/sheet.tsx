4735e602349d05c0d8f881712d3cf33e87d8ed3f
Sombrio <sombrio@sombrasoft.dev>
2024-05-14 10:40:52 -0300

    removed unused import

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
-import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
