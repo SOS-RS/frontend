eb380e26498d3a40d68966c015ca6b8d8d9902dc
Sombrio <sombrio@sombrasoft.dev>
2024-05-14 12:18:30 -0300

    added logout

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,15 +1,13 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { IBurgerMenu, IPartnerLink } from "./types";
-import { forwardRef } from "react";
-import { CircleHelp, CirclePlus, Link as LinkIcon, Menu, User } from "lucide-react";
+import { CircleHelp, CirclePlus, DoorClosed, DoorOpen, Link as LinkIcon, Menu, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
-const BurgerMenu = forwardRef<HTMLDivElement, IBurgerMenu>((props) => {
+const BurgerMenu = (props: IBurgerMenu) => {
   const { session } = props;
   const partnerLinks: IPartnerLink[] = [
     // {
@@ -20,10 +18,17 @@ const BurgerMenu = forwardRef<HTMLDivElement, IBurgerMenu>((props) => {
   return (
     <Sheet>
       <SheetTrigger><Menu color="white" className="ml-2 mr-2" /></SheetTrigger>
-      <SheetContent side={"left"} style={{ marginTop: 56 }}>
-        <SheetDescription className="ml-2 pt-7">
+      <SheetContent side={"left"} className="pt-[96px] flex flex-col">
+        <div className="flex">
           <ul className="flex flex-col text-base">
-            {session && <li className="inline-flex items-center mb-5"><User className="mr-2" />Olá, Fulano</li>}
+            {
+            //TODO: Uncomment after implementing authentication/login
+            /* {session
+            ? <li className="inline-flex items-center mb-5"><User className="mr-2" />Olá, Fulano</li>
+            : <Link to={"/signin"} className="hover:font-semibold">
+              <li className="inline-flex items-center mb-5"><DoorClosed className="mr-2" />Entrar</li>
+            </Link>
+          } */}
             <Link to={"https://forms.gle/2S7L2gR529Dc8P3T9"} target="_blank" className="hover:font-semibold">
               <li className="inline-flex items-center mb-5"><CirclePlus className="mr-2" />Cadastrar Abrigo</li>
             </Link>
@@ -33,22 +38,29 @@ const BurgerMenu = forwardRef<HTMLDivElement, IBurgerMenu>((props) => {
           </ul>
           {!!partnerLinks.length &&
             <>
-              <div className="mt-5 text-sm text-gray-500">Links Úteis</div><ul className="flex flex-col text-base">
+              <div className="mt-5 text-sm text-gray-500">Links Úteis</div>
+              <ul className="flex flex-col text-base">
                 {partnerLinks.map((link, index) => (
                   <Link key={index} to={link.url} target="_blank" className="hover:font-semibold">
                     <li className="inline-flex items-center mt-5"><LinkIcon className="mr-2" />{link.name}</li>
                   </Link>
                 ))}
               </ul>
-            </>}
-        </SheetDescription>
+            </>
+          }
+        </div>
+        {
+          session &&
+          <div className="mt-auto mb-8">
+            <Link to={"/logout"} className="hover:font-semibold">
+              <span className="inline-flex items-center"><DoorOpen className="mr-2" /> Sair</span>
+            </Link>
+          </div>
+        }
       </SheetContent>
+
     </Sheet>
   );
-});
+};
 
 export { BurgerMenu };
-
-<Link to={"https://forms.gle/2S7L2gR529Dc8P3T9"} target="_blank" className="hover:font-semibold">
-  <li className="inline-flex items-center mt-5"><LinkIcon className="mr-2" />Link Projeto Parceiro</li>
-</Link>
