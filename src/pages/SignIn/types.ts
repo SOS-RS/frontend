44b5fcd642e800e0ab27976f720b1e4fff88b50c
Sombrio <sombrio@sombrasoft.dev>
2024-05-14 13:35:25 -0300

    deleted unused file

--- src/pages/SignIn/types.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-const signInFormSchema = Yup.object({
-  login: Yup.string().min(1, "Este campo deve ser preenchido"),
-  password: Yup.string().min(1, "Este campo deve ser preenchido"),
-}).strict();
-
-
-export { signInFormSchema };
