6818198677b7b3149f4afcc7ca2a8285ae777b3c
marcoDmc <marcodamasceno07@gmail.com>
2024-05-14 11:07:03 -0300

    feat: created method function that takes the screen size in real time

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -96,6 +96,26 @@ const Home = () => {
     );
   }, [refresh, filterData, shelters.filters, shelters.page, shelters.perPage]);
 
+
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+
   return (
     <div className="flex flex-col h-screen items-center">
       {isModalOpen && (
