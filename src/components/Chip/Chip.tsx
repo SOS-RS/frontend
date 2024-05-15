77fcca303bf9914a9b9b4ecdde842fdac3cf72d4
NedcloarBR <nedcloar1@hotmail.com>
2024-05-15 19:59:24 -0300

    feat(dark-mode): update text color in Chip Component white -> black more easier to read

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -6,7 +6,7 @@ const Chip = React.forwardRef<HTMLDivElement, IChipProps>((props, ref) => {
 	const { label, className, variant = 'info', ...rest } = props;
 
   const variants = cva(
-    'px-4 py-1.5 font-medium text-sm md:text-md rounded-2xl dark:text-white',
+    'px-4 py-1.5 font-medium text-sm md:text-md rounded-2xl dark:text-black',
     {
       variants: {
         variant: {
