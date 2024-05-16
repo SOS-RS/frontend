7876d887dd43525f3a86539faf4bc972a6eb31ac
Murilo Brandini Zague <zaguemurilo7@gmail.com>
2024-05-16 09:26:54 -0300

    Adicionando acessibilidade

--- src/pages/AboutUs/frontItems.ts
+++ src/pages/AboutUs/frontItems.ts
@@ -5,63 +5,74 @@ const frontItems: IServicedFrontInfoProps[] = [
     icon: '🚰',
     title: 'Água',
     description: 'Atender demandas urgentes de água;',
+    'aria-label': 'Serviço de Água: Atender demandas urgentes de água.'
   },
   {
     icon: '🥘',
     title: 'Cozinhas solidárias',
     description:
       'Auxiliar na operação das cozinhas com o envio de suprimentos e distribuição das marmitas;',
+    'aria-label': 'Serviço de Alimentação: Envio e distribuição de suprimentos.'
   },
   {
     icon: '🏠',
     title: 'Conexões',
     description:
       'Gerir dados confiáveis sobre a demanda de abrigos e a disponibilidade de doações;',
+    'aria-label': 'Conexões: Gerir dados confiáveis sobre demandas e disponibilidades de abrigos e doações.'  
   },
   {
     icon: '️❤️',
     title: 'Comunidades',
     description: 'Mapear e integrar lideranças comunitárias;',
+    'aria-label': 'Comunidades: Mapear e integrar lideranças comunitárias.'
   },
   {
     icon: '📣',
     title: 'Comunicação',
     description:
       'Manter as informações sobre a iniciativa atualizadas e disponíveis a todos;',
+    'aria-label': 'Comunicação: Manter a atualização de informações.'
   },
   {
     icon: '📍',
     title: 'Dados e Geolocalização',
     description:
       'Fornecer dados públicos sobre as enchentes e as demandas do cenário de crise com o apoio do IPH UFRGS e nossas operações de voluntários;',
+    'aria-label': 'Localizações: Fornecimento de dados sobre enchentes e as demandas do cenário de crise com o apoio do IPH UFRGS.'
   },
   {
     icon: '🎯',
     title: 'Expansão',
     description: 'Estruturar o crescimento da operação e parcerias;',
+    'aria-label': 'Expansão: Estruturar o crescimento da operação e parcerias.'
   },
   {
-    icon: '️🚗',
+    icon: '️🚚',
     title: 'Logística',
     description:
       'Conectar o transporte dos centros de distribuição e triagem aos abrigos;',
+    'aria-label': 'Logística: Conectar transportes aos centros de distribuição.'
   },
   {
     icon: '🧒🏽',
     title: 'Olhar para a criança',
     description: 'Proteger e oferecer apoio às crianças em abrigos;',
+    'aria-label': 'Olhar para a criança: Fornecimento de proteção às crianças em abrigos.'
   },
   {
     icon: '️🧑🏽‍🤝‍🧑🏽',
     title: 'Pessoas',
     description:
       'Recrutar, acolher, orientar, treinar e direcionar novos voluntários e padrinhos/madrinhas;',
+    'aria-label': 'Recrutar, acolher, orientar, treinar e direcionar novos voluntários e padrinhos/madrinhas.'  
   },
   {
     icon: '🖥️',
     title: 'Tecnologia',
     description:
       'Desenvolver uma solução digital a partir de dados públicos confiáveis.',
+    'aria-label': 'Tecnologia: Desenvolvimento de soluções tecnológicas com base em dados confiáveis.'  
   },
 ];
 
