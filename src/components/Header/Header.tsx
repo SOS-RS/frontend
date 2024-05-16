008121d0dfdcb80e6cb9ce44d74cecbcc6bf92f7
Matheus Gabriel Pereira do Couto <matheus.gabriel.couto@hotmail.com>
2024-05-15 21:39:04 -0400

    fix: z-index header and validation tag quantity in shelterCategoryItems

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ const Header = React.forwardRef<HTMLDivElement, IHeader>((props, ref) => {
     <div
       ref={ref}
       className={cn(
-        'bg-red-600 flex h-[56px] justify-between items-center text-white p-3 gap-2 w-full z-[60]',
+        'bg-red-600 flex h-[56px] justify-between items-center text-white p-3 gap-2 w-full z-[40]',
         className
       )}
       {...rest}
