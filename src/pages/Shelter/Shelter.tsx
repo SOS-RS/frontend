b7df99fc256fcd473f2af64dd9635d34043ef901
Marciel Torres <marcielribeirotorres@gmail.com>
2024-05-16 15:09:03 -0300

    Adicionando informação sobre garantir a privacidade das pessoas nos abrigos

--- src/pages/Shelter/Shelter.tsx
+++ src/pages/Shelter/Shelter.tsx
@@ -123,7 +123,9 @@ const Shelter = () => {
         </div>
         <div className="p-4">
           <CardAboutShelter shelter={shelter} />
+          <p className='p-1'>Lembre-se: é probibido tirar fotos ou filmar dentro dos abrigos, garanta a privacidade das pessoas nesse momento de vulnerabilidade.</p>
         </div>
+        
         <div className="flex justify-between p-4 items-center">
           <h1 className="font-semibold text-[18px]">Itens do abrigo</h1>
           <div className="flex gap-2 items-center ">
