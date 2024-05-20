3e05fd8cb4521171909b1016cacd9f792b0f7f99
leonardo <lmedeiros045@gmail.com>
2024-05-20 15:20:04 -0300

    removendo necessidade de remover overflow-x:hidden no global.css

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -14,6 +14,7 @@ const BackToTop = () => {
     root.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+
   document.getElementById('root')?.addEventListener('scroll', (e) => {
     if (e.target === null) {
       return;
@@ -28,7 +29,7 @@ const BackToTop = () => {
     }
   });
 
-  
+
   return (
     isVisible && (
       <button
