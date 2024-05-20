6c9c5e1bdded92c54ee68405a31e0a1d495c888e
Danilo Romano <danilo.romano@datagrupo.com.br>
2024-05-20 14:16:14 -0300

    chore: rearranging header semantics in AboutUs page.

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -32,15 +32,15 @@ const AboutUs = () => {
     <div className="flex flex-col h-screen items-center">
       <Header title="SOS Rio Grande do Sul" startAdornment={<BurgerMenu />} />
       <main className="flex flex-col gap-4 p-4 max-w-4xl pb-8 w-full">
-        <h2 className="text-4xl pt-4 font-semibold !text-zinc-900">
+        <h1 className="text-4xl pt-4 font-semibold !text-zinc-900">
           Sobre nós
-        </h2>
-        <h4 className="text-lg text-muted-foreground font-medium">
+        </h1>
+        <small className="text-lg text-muted-foreground font-medium">
           Conheça a história do projeto SOS RS
-        </h4>
-        <h3 className="text-2xl font-medium text-muted-foreground mt-8">
+        </small>
+        <h2 className="text-2xl font-medium text-muted-foreground mt-8">
           Como tudo começou
-        </h3>
+        </h2>
         <p className="text-justify text-md md:text-lg font-medium [&>a]:text-blue-500 [&>a]:hover:text-blue-600 [&>a]:active:text-blue-700">
           Iniciado no domingo (04/05) e concluído na segunda (05/05), após 18
           horas seguidas de desenvolvimento, nosso webapp <b>SOS RS 🛟</b>,
@@ -94,9 +94,9 @@ const AboutUs = () => {
           nosso projeto alcançou <b>400 estrelas no GitHub</b> e foi{' '}
           <b>forkeado mais de 150 vezes!</b>
         </p>
-        <h3 className="text-2xl font-medium text-muted-foreground mt-8">
+        <h2 className="text-2xl font-medium text-muted-foreground mt-8">
           Nossos parceiros
-        </h3>
+        </h2>
         <p className="text-justify font-medium text-md md:text-lg">
           Formamos parcerias sólidas com mais de 1400 voluntários, fortalecendo
           ainda mais nossa missão. Hoje, comemoramos números que falam por si.
@@ -141,9 +141,9 @@ const AboutUs = () => {
           O grupo de voluntários do SOS RS já soma mais de 2100 pessoas e cerca
           de 126 mil horas de trabalho desde a criação da iniciativa.
         </p>
-        <h3 className="text-2xl font-medium text-muted-foreground mt-4">
+        <h2 className="text-2xl font-medium text-muted-foreground mt-4">
           Contribuidores da plataforma ({contributors.length})
-        </h3>
+        </h2>
         <div className="flex flex-wrap gap-2">
           {loading ? (
             <Loader className="w-5 h-5 animate-spin" />
