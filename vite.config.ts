d208bf16707b8d6819ddb395b6a1850e5287f45c
José Geraldo D. F <fagundesjg@outlook.com>
2024-05-23 17:59:51 -0300

    feat: added hmac and obfuscate api module (#294)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,50 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig, Plugin } from 'vite';
+import JavaScriptObfuscator from 'javascript-obfuscator';
+
+function obfuscate(includes: string[] = []): Plugin {
+  return {
+    name: 'vite-javascript-obfuscator',
+    apply: 'build',
+    enforce: 'post',
+    generateBundle(options, bundle) {
+      for (const fileName in bundle) {
+        const chunk = bundle[fileName];
+        if (
+          chunk.type === 'chunk' &&
+          fileName.endsWith('.js') &&
+          includes.some((f) => fileName.includes(f))
+        ) {
+          const obfuscatedCode = JavaScriptObfuscator.obfuscate(chunk.code, {
+            compact: true,
+            numbersToExpressions: true,
+            stringArrayShuffle: true,
+            splitStrings: true,
+            stringArrayThreshold: 1,
+          }).getObfuscatedCode();
+          chunk.code = obfuscatedCode;
+        }
+      }
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), obfuscate(['api'])],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          api: ['./src/api'],
+        },
+      },
     },
   },
 });
