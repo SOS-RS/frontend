9bef761a168a54bc19c690a2f73078168b705a08
johnviti <jvsa5@aluno.ifal.edu.br>
2024-06-02 01:34:20 -0300

    Fix: check filterData.priorities is array

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -93,7 +93,7 @@ const Home = () => {
   }, [refresh, filterData, shelters.filters, shelters.page, shelters.perPage, factorySearchArgs]);
 
   useEffect(() => {
-    if (filterData.search || filterData.cities.length > 0 || filterData.priorities || filterData.shelterStatus.length > 0 || filterData.supplyCategoryIds.length > 0 || filterData.supplyIds.length > 0){
+    if (filterData.search || filterData.cities.length > 0 || filterData.priorities.length > 0 || filterData.shelterStatus.length > 0 || filterData.supplyCategoryIds.length > 0 || filterData.supplyIds.length > 0){
       setSearchParams(qs.stringify(filterData));
       refresh({ params: { search: qs.stringify(filterData) } });
     }
