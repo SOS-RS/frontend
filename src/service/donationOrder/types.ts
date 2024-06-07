df97f8cb9fa5e8c59b803fb66822ed01c02181ba
mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-06-07 19:52:43 -0300

    fix(sync): merge adjust

--- src/service/donationOrder/types.ts
+++ src/service/donationOrder/types.ts
@@ -1,3 +1,4 @@
+import { SupplyMeasure } from '@/hooks/useShelter/types';
 export interface IDonateItem {
   id: string;
   quantity: number;
@@ -21,4 +22,29 @@ export enum DonateOrderStatus {
   Pending = 'Pending',
   Canceled = 'Canceled',
   Complete = 'Complete',
+}
+
+export interface IDonateOrderItem {
+  id: string;
+  status: DonateOrderStatus;
+  userId: string;
+  shelter: IDonateOrderShelter;
+  donationOrderSupplies: IDonateOrderItemSupply[];
+  createdAt: string;
+  updatedAt?: string | null;
+}
+
+export interface IDonateOrderItemSupply {
+  quantity: number;
+  supply: IDonateOrderSupply;
+}
+
+export interface IDonateOrderShelter {
+  id: string;
+  name: string;
+}
+
+export interface IDonateOrderSupply {
+  name: string;
+  measure: SupplyMeasure;
 }
